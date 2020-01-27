f257039129ae1a89d3e4e89573b4ba35491a2470
Tarun Chauhan <artisan.tarun@gmail.com>
2020-01-27 21:50:43 +0530

    tarun

---
+++ scripts/tarun.js
@@ -0,0 +1,11 @@
+function paraUpdate(){
+    document.getElementById('para').innerHTML = "This is the updated text and this text will be lost once this page is refreshed";
+}
+
+function paraWriteDisp(){
+    document.write("Hello, Hope you are enjoying JavaScript Language<br>To view data again refresh the page");
+}
+
+function myAlert(){
+    window.alert("Hello, Hope you are enjoying learning JavaScript");
+}
